5cc16b8090554cbde46e11859dc8267111b680d4
knight42 <anonymousknight96@gmail.com>
2020-05-11 19:29:33 +0800

    update

--- install.sh
+++ install.sh
@@ -8,10 +8,11 @@ deb http://mirrors.aliyun.com/debian unstable main contrib non-free
 EOF
 
 apt update && apt install -y --no-install-recommends ca-certificates
-sed -i 's|http|https|' /etc/apt/sources.list && apt install -y --no-install-recommends wget curl vim jq git less apache2-utils procps \
-    tcpdump ncat iperf3 iproute2 bmon sysbench stress-ng dnsutils sockperf siege qperf telnet
+sed -i 's|http|https|' /etc/apt/sources.list && apt install -y wget curl vim jq git less apache2-utils procps \
+    tcpdump ncat iperf3 iproute2 bmon sysbench stress-ng dnsutils sockperf siege qperf telnet iputils-ping ipvsadm \
+    redis-tools mysql-client
 
-wget https://github.com/fortio/fortio/releases/download/v${FORTIO_VERSION}/fortio_${FORTIO_VERSION}-1_amd64.deb
+wget -q https://github.com/fortio/fortio/releases/download/v${FORTIO_VERSION}/fortio_${FORTIO_VERSION}-1_amd64.deb
 dpkg -i fortio_${FORTIO_VERSION}-1_amd64.deb
 rm fortio_${FORTIO_VERSION}-1_amd64.deb
 
